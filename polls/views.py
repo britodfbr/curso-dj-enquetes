7626c7fecbcc04dc434ca77a21e28b4b04279a8e
@britodfbr <britodfbr@gmail.com>
2020-10-16 11:49:42 -0300

    Aplicado template em views.detail

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question
 from django.template import loader
+from django.http import Http404
 
 
 def index(request):
@@ -12,7 +13,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
